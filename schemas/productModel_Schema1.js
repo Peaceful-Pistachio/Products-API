db6b363811b9fe8c37eb40c9a29021f86620092e
Sean Welch <sean.welch@me.com>
2022-01-11 13:13:04 -0800

    removed seperate schemas in favor of one large paroduct schema

--- schemas/productModel_Schema1.js
+++ schemas/productModel_Schema1.js
@@ -1,27 +1,5 @@
 const mongoose = require('mongoose');
 
-const SKUSchema = new.mongoose.Schema({
-  skuNumber: { type: Number, unique: true },
-  quantity: Number,
-  size: String
-})
-
-const StyleSchema = new.mongoose.Schema({
-  style_id: { type: Number, unique: true },
-  name: String,
-  original_price: Number,
-  sale_price: Number,
-  default: Boolean,
-  photos: [{
-    thumbnail_url: String,
-    url: String
-  }],
-  skus: [SKUSchema]
-})
-
-const RelatedSchema = new.mongoose.Schema({
-  product_id: Number
-})
 
 const ProductSchema = new.mongoose.Schema({
   product_id: { type: Number, index: true, unique: true },
@@ -30,18 +8,25 @@ const ProductSchema = new.mongoose.Schema({
   description: String,
   category: String,
   default_price: Number,
-  related_products: [RelatedSchema],
-  styles: [StyleSchema],
   features: [{
     feature: String,
     value: String
+  }],
+  related_products: [Number],
+  styles: [{
+    style_id: { type: Number, unique: true },
+    name: String,
+    original_price: Number,
+    sale_price: Number,
+    default: Boolean,
+    photos: [{
+      thumbnail_url: String,
+      url: String
+    }],
+    skus: [{ sku: { type: Number, unique: true },quantity: Number, size: String }]
   }]
 });
 
 const Product = mongoose.model('Product', ProductSchema);
-const Style = mongoose.model('Style', StyleSchema);
-const SKU = mongoose.model('SKU', SKUSchema);
-const Related = mongoose.model('Related', RelatedSchema);
 
-
-module.exports = { Product, Style, SKU, Related };
+module.exports = Product;
