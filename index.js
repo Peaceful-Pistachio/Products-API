291ad30e73ac4a9459f7c22bbf8541863960fd74
Sean Welch <sean.welch@me.com>
2022-01-19 07:22:13 -0800

    implemented cluster fuctionalitu for express server

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+let port = process.env.PORT || 3000;
 
-//Import Routes
-const productRouter = require('./controller/productRoutes');
+const cluster = require('cluster');
+const totalCPUs = require('os').cpus().length;
 
-//Server Settings
-let port = process.env.port || 3000;
+if (cluster.isMaster) {
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
 
-//Middleware Setup
-app.use('/products', productRouter);
-app.use(express.json());
-app.use(cors());
+  cluster.on('exit', (worker, code, signal) => {
+    cluster.fork();
+  })
 
-//Basic Routes for Testing
-app.get('/hello-world', (req, res) => {
-  res.json({ message: 'Hello World!' })
-});
+} else {
 
-let htmlMessage = `<h1>Welcome!</h1><p>You have reached the HR-SDC products API. To consume this REST API, please visit one of the following links:</p>
+  //Import Routes
+  const productRouter = require('./controller/productRoutes');
+
+  //Middleware Setup
+  app.use('/products', productRouter);
+  app.use(express.json());
+  app.use(cors());
+
+  //Basic Routes for Testing
+  app.get('/hello-world', (req, res) => {
+    res.json({ message: 'Hello World!' })
+  });
+
+  let htmlMessage = `<h1>Welcome!</h1><p>You have reached the HR-SDC products API. To consume this REST API, please visit one of the following links:</p>
   <ul>
     <li>
     <a href="/products">Products</a>
@@ -35,20 +47,28 @@ let htmlMessage = `<h1>Welcome!</h1><p>You have reached the HR-SDC products API.
   </ul>`;
 
   //Welcome Page
-app.get('/', (req, res) => {
-  res.setHeader('Content-type','text/html');
-  res.status(200).send(htmlMessage);
-});
+  app.get('/', (req, res) => {
+    res.setHeader('Content-type', 'text/html');
+    res.status(200).send(htmlMessage);
+  });
 
-app.get('/loaderio-b4d0ac2c41481e9702d38f174f5095d5/', (req, res) => {
-  res.status(200).send('loaderio-b4d0ac2c41481e9702d38f174f5095d5');
-});
+  app.get('/loaderio-b4d0ac2c41481e9702d38f174f5095d5/', (req, res) => {
+    res.status(200).send('loaderio-b4d0ac2c41481e9702d38f174f5095d5');
+  });
 
-//Not Found Middleware
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  //Not Found Middleware
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  app.listen(port, (err) => {
+    err ?
+      console.log(error) :
+      console.log("Server listening on port:", port);
+  });
+
+}
 
 module.exports = { app, port }
--- server.js
+++
@@ -1,6 +0,0 @@
-const server = require('./index');
-server.app.listen(server.port, (err) => {
-  err ?
-    console.log(error) :
-    console.log("Server listening on port:", server.port);
-});
